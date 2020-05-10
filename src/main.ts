b40d1499500833584028295b5f92df3b4f8e9917
Dominique Fober <fober@grame.fr>
2020-05-10 19:33:47 +0200

    warn user on explorer and edge

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@
 ///<reference path="editorGlue.ts"/>
 ///<reference path="editor.ts"/>
 
+
+
 //------------------------------------------------------------------------
 var editor : InscoreEditor;
 var glue = new EditorGlue();
@@ -9,7 +11,12 @@ glue.start().then (() => {
 	editor = new InscoreEditor("code"); 
 	editor.initialize();
 	$("#version").text( inscore.versionStr() ); 
-	setTimeout (() => $("#loading").remove(), 1000);
+	setTimeout (() => $("#loading").remove(), 500);
+	var ua = window.navigator.userAgent;
+	let warnuser = (ua.indexOf('MSIE ') >= 0) || (ua.indexOf('Trident') >= 0) || (ua.indexOf('Edge') >= 0);
+	if (true)
+		setTimeout (() => alert("WARNING!\nINScore JS doesn't work properly with Explorer or Edge.\nPreferably use Chrome or Firefox."), 700);
+	
 } );
 
 var showlog = function(status: boolean) {
@@ -18,3 +25,4 @@ var showlog = function(status: boolean) {
 	else
 		$("#editornav").click();
 }
+
